f86b26eccf6481e19b7ac58ddf4869bc6c32f2ce
Mocanu, Radu <radu.mocanu@finastra.com>
2024-01-03 14:06:03 +0200

    initial commit

---
+++ src/config/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDp_orYIVzLMVi-ArqgGuFgBw5m4FZbvMU",
+    authDomain: "lawfirm-b6f3c.firebaseapp.com",
+    projectId: "lawfirm-b6f3c",
+    storageBucket: "lawfirm-b6f3c.appspot.com",
+    messagingSenderId: "644948142660",
+    appId: "1:644948142660:web:a6f1074623a1358214aa11"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
